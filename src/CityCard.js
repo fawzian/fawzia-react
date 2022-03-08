185d5b454d1ba76ebc7f6ddfdad1d216ba500065
FAWZIA NUR <FAWZIANUR@GMAIL.com>
2022-03-07 23:12:13 -0500

    upload changes

--- src/CityCard.js
+++ src/CityCard.js
@@ -5,13 +5,17 @@ export default function CityCard( { city } ) {
     return (
         <div className="cityCard">
         <h2>{city["city"]}</h2> 
-        <h2><span className="subheading">Location ID: </span> {city["locationId"]}</h2> <br/>
-        <p><span className="subheading"> location: </span> {city["location"]}</p>
-        <p><span className="subheading">entity: </span>{city["entity"]}</p>
-        <p><span className="subheading">parameter: </span>{city["parameter"]}</p>
-        <p><span className="subheading">sensor type: </span>{city["sensorType"]}</p>
-        <p><span className="subheading">unit: </span>{city["unit"]}</p>
-        <p><span className="subheading">value: </span>{city["value"]}</p>
+        <p><span className="subheading"> Location: </span> {city["location"]}</p>
+        <p><span className="subheading">Location ID: </span> {city["locationId"]}</p>
+        <br/>
+
+        <hr/>
+        <br/>
+        <p><span className="subheading">Entity: </span>{city["entity"]}</p>
+        <p><span className="subheading">Parameter: </span>{city["parameter"]}</p>
+        <p><span className="subheading">Sensor Type: </span>{city["sensorType"]}</p>
+        <p><span className="subheading">Unit: </span>{city["unit"]}</p>
+        <p><span className="subheading">Value: </span>{city["value"]}</p>
                   
         </div>
     )
